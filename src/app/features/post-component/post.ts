1daafd9fb843e5ca1fc5118e6ce1e146f42c67a8
Ayushi <ayushipathak0330@gmail.com>
2026-04-11 14:30:44 +0530

    updatd code for level 5

--- src/app/features/post-component/post.ts
+++ src/app/features/post-component/post.ts
@@ -3,22 +3,31 @@ import { Post } from '../../core/interface/post';
 import { ApiService } from '../../core/services/api';
 import { Card } from '../../shared/card/card';
 import { Loader } from '../../shared/loader/loader';
-import { delay } from 'rxjs';
+import { delay,debounceTime,distinctUntilChanged } from 'rxjs';
 import { error } from 'console';
+import { FormControl, Validators ,ReactiveFormsModule } from '@angular/forms';
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-post',
-  imports: [Card,Loader],
+  imports: [Card,Loader,ReactiveFormsModule],
   templateUrl: './post.html',
   styleUrl: './post.css',
 })
-export class PostComponent {
+
+export class PostComponent implements OnInit{
+  public search=new FormControl('')
   public isLoading:boolean=true
   public errorMsg:string=""
   public errorCode:number=0
-  public store_post: Post[] = [];
-  constructor(private _api: ApiService) {
-    _api.getPosts().pipe(delay(5000)).subscribe((post) => {
+  public store_post: Post[] = []
+  public filteredPosts:Post[]=[]
+
+  constructor(private _api: ApiService) {}
+  ngOnInit(){
+
+    this._api.getPosts().pipe(delay(5000)).subscribe((post) => {
       this.store_post=post
+      this.filteredPosts=post
       this.isLoading=false
     },(error)=>{
       this.errorMsg=error.message
@@ -28,7 +37,7 @@ export class PostComponent {
       const code=Math.floor(this.errorCode/100)
       switch (code){
         case 1:
-           this.errorMsg="Your request is being processed.Please wait.."
+        this.errorMsg="Your request is being processed.Please wait.."
         break;
       case 2:
         this.errorMsg="Request successfull.Data loaded properly"
@@ -47,8 +56,23 @@ export class PostComponent {
         break;
 
       }
-
-    });
-
+      
+    }
+    
+  );
+    this.search.valueChanges.pipe(distinctUntilChanged()).subscribe((searchTitle) => {
+        this.filterPosts(searchTitle || '');
+      });
+  }
+private filterPosts(searchTerm:string):void{
+  const term=searchTerm.toLowerCase().trim();
+  if(!term){
+    this.filteredPosts=this.store_post} 
+    else{
+      this.filteredPosts=this.store_post.filter(post=> post.title.toLowerCase().includes(term));
+    }
   }
 }
+
+
+
